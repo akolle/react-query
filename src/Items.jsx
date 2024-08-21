2522e428ff379be3a31465b13edef063a0858288
akolle <andykoller55@yahoo.com>
2024-08-21 17:14:46 -0400

    refactor with custom hooks

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,27 +1,20 @@
-import { useQuery } from '@tanstack/react-query'
+import { useFetchTasks } from './ReactQueryCustomHooks'
 import SingleItem from './SingleItem'
-import customFetch from './utils'
 
 const Items = ({ items }) => {
-  const { isLoading, data, error, isError } = useQuery({
-    queryKey: ['tasks'],
-    queryFn: async () => {
-      const { data } = await customFetch.get('/')
-      return data
-    },
-  })
+  const { isLoading, isError, data } = useFetchTasks()
 
   if (isLoading) {
     return <p style={{ marginTop: '1rem' }}>Loading...</p>
   }
 
-  // if (isError) {
-  //   return <p style={{ marginTop: '1rem' }}>There was an error...</p>
-  // }
-
-  if (error) {
-    return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>
+  if (isError) {
+    return <p style={{ marginTop: '1rem' }}>There was an error...</p>
   }
+
+  // if (error) {
+  //   return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>
+  // }
 
   return (
     <div className="items">
